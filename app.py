acb3784e6bd9a804addd69023f7ad6389d77ede1
michael-basweti <baswetima@gmail.com>
2021-05-23 07:53:42 +0300

    get frame children

--- app.py
+++ app.py
@@ -1,22 +1,38 @@
-import tkinter as tk
+from tkinter import *
 from tkinter import filedialog, Text
 import os
 
-root = tk.Tk()
+root = Tk()
+apps = []
 
 def addApp():
-    filename = filedialog.askopenfilename(initialdir="/", title="Select File", filetypes=(("executables",".exe"),("all files","*")))
+    # for widget in frame.winfo_children():
+    #     widget.destory()
+    children = frame.winfo_children()
+    for label in children:
+        print(label)
+        label.destroy()
 
-canvas = tk.Canvas(root, height=700, width=700, bg="#439cd3")
+
+    filename = filedialog.askopenfilename(initialdir="/", title="Select File", filetypes=(("executables",".exe"),("all files","*")))
+    apps.append(filename)
+
+
+    for app in apps:
+        label = Label(frame, text=app, bg='gray')
+        label.pack()
+        print(label)
+
+canvas = Canvas(root, height=700, width=700, bg="#439cd3")
 canvas.pack()
 
-frame = tk.Frame(root, bg="white")
+frame = Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-openFile = tk.Button(root, text='Open File', padx=10,pady=5,fg="white", bg="#439cd3", command=addApp)
+openFile = Button(root, text='Open File', padx=10,pady=5,fg="white", bg="#439cd3", command=addApp)
 openFile.pack()
 
-runApps = tk.Button(root, text='Run Apps', padx=10,pady=5,fg="white", bg="#439cd3")
+runApps = Button(root, text='Run Apps', padx=10,pady=5,fg="white", bg="#439cd3")
 
 runApps.pack()
 
